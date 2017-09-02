84de5998fc515c13e64f8b4fee9519c4f33789a0
Sagarika Kundu <kundusagarika@yahoo.com>
2017-09-02 10:51:34 +0530

    Add files via upload

---
+++ ideone_zApnxQ.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(char *x, char *y){
+	char temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void permute(char *a, int l, int r){
+int i;
+if(l == r)
+	{
+		for(i=0; i<=r; i++)
+			cout << a[i];
+		cout << "\n";
+	}
+else{
+	
+	for(i= l; i <= r; i++ ){
+		swap((a+l), (a+i));
+		permute(a, l+1, r);
+		swap((a+l), (a+i));
+						}
+	}
+}
+
+int main() {
+	cout << "enter no of characters\n";
+	int n,i;
+	cin >> n;
+	char a[n];
+	for(i =0; i <n; i++)
+		cin >> a[i];
+	permute(a, 0, n-1);
+	return 0;
+}
