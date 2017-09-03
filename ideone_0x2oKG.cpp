a8746acb70aa8fc305a59c34ec4ff53220cf5877
Sagarika Kundu <kundusagarika@yahoo.com>
2017-09-04 01:28:03 +0530

    Add files via upload

---
+++ ideone_0x2oKG.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+bool marked[10000];
+vector <pair<long long, int> > adj[10000];
+
+long long prim(int x)
+{
+    priority_queue< pair<long long, int>, vector< pair<long long, int> >, greater<pair<long long, int> > > Q;
+    int y;
+    long long minimumCost = 0;
+    pair<long long, int> p;
+    Q.push(make_pair(0, x));
+    while(!Q.empty())
+    {
+        // Select the edge with minimum weight
+        p = Q.top();
+        Q.pop();
+        x = p.second;
+        // Checking for cycle
+        if(marked[x] == true)
+            continue;
+        minimumCost += p.first;
+        marked[x] = true;
+        for(int i = 0;i < adj[x].size();++i)
+        {
+            y = adj[x][i].second;
+            if(marked[y] == false)
+                Q.push(adj[x][i]);
+        }
+    }
+    return minimumCost;
+}
+
+int main()
+{
+    int nodes, edges, x, y;
+    long long weight, minimumCost;
+    cin >> nodes >> edges;
+    for(int i = 0;i < edges;++i)
+    {
+        cin >> x >> y >> weight;
+        adj[x].push_back(make_pair(weight, y));
+        adj[y].push_back(make_pair(weight, x));
+    }
+    // Selecting 1 as the starting node
+    minimumCost = prim(1);
+    cout << minimumCost << endl;
+    return 0;
+}
