38e1ec6c208bf1067f7776bd2e4c9355a166c810
Sagarika Kundu <kundusagarika@yahoo.com>
2017-09-07 13:42:36 +0530

    Update radixLexiSort.cpp

--- radixLexiSort.cpp
+++ radixLexiSort.cpp
@@ -1,12 +1,15 @@
+//here we have assumed that the max no of characters in each word will not be more than 100
+//we have considered only alphabets as inputs.
+//if we want to sort according to ascii we shall not subtract 65 from the index value of freq.
 #include <iostream>
 using namespace std;
 void countSort(char a[][100], int n, int len, int m){
 	int i,j;
-	int freq[122] = {0};
+	int freq[57] = {0};
 	int output[n][len]= {0};
 	for(i=0;i<n;i++)
 		freq[(a[i][m-1])-65]++;
-	for(i=1;i<122;i++)
+	for(i=1;i<57;i++)
 		freq[i]+=freq[i-1];
 	for(i=n-1;i>=0;i--){
 		for(j=0; j<len; j++)
